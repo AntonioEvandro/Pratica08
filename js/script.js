9e47e11fc95dcb6dd0c41259e17b2eb13df6d37e
Antonio Evandro <100598553+AntonioEvandro@users.noreply.github.com>
2022-06-03 17:14:00 -0300

    incompleto
    me perdi na explicação

--- js/script.js
+++ js/script.js
@@ -1,5 +1,31 @@
 const pais = document.querySelector("#pais")
 
-let selecionado = pais.options.selectIndex
+let selecionado = pais.options.selectedIndex
 
 console.log(pais.options[selecionado])
+
+const passporte = document.querySelector("#passaporte")
+
+const padraoPassaporte = new RegExp("[a-zA-Z]{2}[0-9]{12}")
+
+const enviar = document.querySelector("#enviar")
+
+const validacaoPassaporte = document.querySelector("#validar-passaporte")
+
+enviar.addEventListener("click", (e)=> {
+    e.preventDefault()
+    e.stopPropagation()
+
+    alert(padraoPassaporte.exec(passporte.value))
+
+    validacaoPassaporte.classList = "validacao"
+
+    if (padraoPassaporte.test(passporte.value)){
+        validacaoPassaporte.classList.add("valido")
+        validacaoPassaporte.innerHTML = "Passaporte válido"
+    }else{
+        validacaoPassaporte.classList.add("invalido")
+        validacaoPassaporte.innerHTML = "Passaporte inválido"
+    }
+
+})
